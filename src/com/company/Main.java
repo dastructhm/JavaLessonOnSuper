47c4c63bf18e55d425d083be7086cb2b401f5155
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-14 16:57:31 +0800

    names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,7 @@
+// Inheritance basically used to achieve dynamic binding or run-time polymorphism in java. Used to access members of a class inherited by the class in which it appears.
+// Allows a subclass to access overridden methods and hidden members of its superclass. The super keyword is also used to forward a call from a constructor to a constructor in the superclass.
+// Also used to specify a lower bound on a type parameter in Generics.
+
 package com.company;
 
 public class Main {
